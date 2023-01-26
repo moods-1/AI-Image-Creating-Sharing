8ecc7718a09890bc11d0f419783c0c6dc7068999
moods-1 <ceemoods@gmail.com>
2023-01-26 17:36:15 -0500

    Debugging 504 error.

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8088;
 
 app.use(
 	cors({
-		origin: 'http://127.0.0.1:5173',
+		origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
 	})
 );
 app.use(logger('dev'));
